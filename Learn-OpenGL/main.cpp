73957d9229577fc5f553e0ae1a5d00b335a55cbb
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-26 12:59:31 +0800

    hello window 2

--- Learn-OpenGL/main.cpp
+++ Learn-OpenGL/main.cpp
@@ -4,6 +4,8 @@
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
 
+void processInput(GLFWwindow* window);
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -30,10 +32,21 @@ int main() {
 
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
 }
