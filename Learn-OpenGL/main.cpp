59689626a07cc2bf6ad8852a84ee58f0bc1aaefa
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-26 18:07:36 +0800

    hello triangle

--- Learn-OpenGL/main.cpp
+++ Learn-OpenGL/main.cpp
@@ -6,6 +6,26 @@
 
 void processInput(GLFWwindow* window);
 
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+
+const char* vertexShaderSource =
+"#version 330 core											 \n"
+"layout(location = 0) in vec3 aPos;							 \n"
+"void main() {												 \n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);		 \n"
+"}															 \n";
+
+const char* fragmentShaderSource =
+"#version 330 core											 \n"
+"out vec4 FragColor;										 \n"
+"void main() {												 \n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);				 \n"
+"}															 \n";
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -30,12 +50,42 @@ int main() {
 
 	glViewport(0, 0, 800, 600);
 
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glUseProgram(shaderProgram);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
