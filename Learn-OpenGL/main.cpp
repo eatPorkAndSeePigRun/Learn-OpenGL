69058adde9842a9a3fc2b54a3acb49bb8fe9c171
eatPorkAndSeePigRun <811302964@qq.com>
2019-11-26 12:57:54 +0800

    hello window 1

--- Learn-OpenGL/main.cpp
+++ Learn-OpenGL/main.cpp
@@ -1,14 +1,39 @@
-#include <iostream>
+#include<iostream>
 
 #define GLEW_STATIC
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
 
 int main() {
 	glfwInit();
-	glfwWindowHint(GLEW_VERSION_MAJOR, 3);
-	glfwWindowHint(GLEW_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// Open GLFW window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My opengl window", NULL, NULL);
+	if (window == NULL) {
+		printf("Open window failed.");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Init GLEW
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		printf("Init GLEW failed");
+		glfwTerminate();
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
